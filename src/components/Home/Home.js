86b348adb7a1ae38f1d3c97c9f2222813d42c14a
Tejas Chauhan <tdchauhan07@gmail.com>
2023-05-31 20:13:30 +0530

    adding footer

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbaar from '../Navbaar/Navbaar'
 import Hero from '../Hero/Hero'
+import Footer from '../Footer/Footer'
 function Home() {
   return (
     <div>
       <Navbaar/>
       <Hero/>
+      <Footer/>
     </div>
   )
 }
